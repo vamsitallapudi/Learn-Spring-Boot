c0cdc0ea624f51402be2dca1762731d0e1dd108a
Vamsi Tallapudi <vamsi@coderefer.com>
2021-05-28 06:47:56 +0530

    created new spring boot proj in intellij

---
+++ secondspringbootproj/src/main/java/com/coderefer/secondspringbootproj/SecondspringbootprojApplication.java
@@ -0,0 +1,14 @@
+package com.coderefer.secondspringbootproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondspringbootprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondspringbootprojApplication.class, args);
+        System.out.println("Welcome to Spring boot Vamsi :)");
+    }
+
+}
---
+++ secondspringbootproj/src/test/java/com/coderefer/secondspringbootproj/SecondspringbootprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderefer.secondspringbootproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondspringbootprojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
