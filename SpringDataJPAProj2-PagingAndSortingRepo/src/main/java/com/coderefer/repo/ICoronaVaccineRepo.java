9f11862dbf9935571adc56d0f192d0db7d7e24f5
Vamsi Tallapudi <vamsi@coderefer.com>
2021-07-09 12:15:52 +0530

    Spring Data JPA Pagination added

--- SpringDataJPAProj2-PagingAndSortingRepo/src/main/java/com/coderefer/repo/ICoronaVaccineRepo.java
+++
@@ -1,9 +0,0 @@
-package com.coderefer.repo;
-
-
-import com.coderefer.entity.CoronaVaccine;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICoronaVaccineRepo extends JpaRepository<CoronaVaccine, Long> {
-
-}
