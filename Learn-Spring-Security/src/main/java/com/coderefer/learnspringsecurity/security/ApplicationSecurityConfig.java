d683fbbcba166c31ff9a6af553b6073a555bf453
Vamsi Tallapudi <vamsi@coderefer.com>
2021-09-08 11:23:19 +0530

    permissions based authentication now working.

--- Learn-Spring-Security/src/main/java/com/coderefer/learnspringsecurity/security/ApplicationSecurityConfig.java
+++ Learn-Spring-Security/src/main/java/com/coderefer/learnspringsecurity/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.coderefer.learnspringsecurity.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.coderefer.learnspringsecurity.security.ApplicationUserPermission.*;
 import static com.coderefer.learnspringsecurity.security.ApplicationUserRole.*;
 
 @Configuration
@@ -32,6 +34,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.GET,"/management/api/**").hasAuthority(COURSE_READ.getPermission())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -45,22 +51,23 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 User.builder()
                         .username("vamsi")
                         .password(passwordEncoder.encode("password"))
-                        .roles(STUDENT.toString())
+                        .authorities(STUDENT.getGrantedAuthorities())
                         .build();
 
         UserDetails krishna =
                 User.builder()
                         .username("krishna")
                         .password(passwordEncoder.encode("password123"))
-                        .roles(ADMIN.toString())
+                        .authorities(ADMIN.getGrantedAuthorities())
                         .build();
 
         UserDetails tallapudi =
                 User.builder()
                         .username("tallapudi")
                         .password(passwordEncoder.encode("password123"))
-                        .roles(ADMINTRAINEE.name())
+                        .authorities(ADMINTRAINEE.getGrantedAuthorities())
                         .build();
+
         return new InMemoryUserDetailsManager(vamsi, krishna, tallapudi);
     }
 }
--- Learn-Spring-Security/src/main/java/com/coderefer/learnspringsecurity/security/ApplicationUserRole.java
+++ Learn-Spring-Security/src/main/java/com/coderefer/learnspringsecurity/security/ApplicationUserRole.java
@@ -1,19 +1,25 @@
 package com.coderefer.learnspringsecurity.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
+
 import  static  com.coderefer.learnspringsecurity.security.ApplicationUserPermission.*;
 
 public enum ApplicationUserRole {
+
     STUDENT(Sets.newHashSet(
     )),
+
     ADMIN(Sets.newHashSet(
             COURSE_READ,
             COURSE_WRITE,
             STUDENT_READ,
             STUDENT_WRITE
     )),
+
     ADMINTRAINEE(Sets.newHashSet(
             COURSE_READ,
             STUDENT_READ
@@ -23,5 +29,19 @@ public enum ApplicationUserRole {
 
     ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
         this.permissions = permissions;
+    }
+
+    public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions =
+                getPermissions().stream()
+                        .map(p -> new SimpleGrantedAuthority(p.getPermission()))
+                        .collect(Collectors.toSet());
+        // because we are going to remove roles from builder, we are compensating this
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return permissions;
     }
 }
