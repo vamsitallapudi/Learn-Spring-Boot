5a756850b5c887b4bc13118070d722c7b364bb70
Vamsi Tallapudi <vamsi@coderefer.com>
2021-09-12 17:11:12 +0530

    adding data from data.sql

--- LearnSpringBoot01/src/main/java/com/coderefer/LearnSpringBoot01/entity/User.java
+++ LearnSpringBoot01/src/main/java/com/coderefer/LearnSpringBoot01/entity/User.java
@@ -1,16 +1,21 @@
 package com.coderefer.LearnSpringBoot01.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@Entity
+@NoArgsConstructor
 public class User {
+    @Id
     private Integer id;
     @Size(min = 2, message = "Name should have atleast 2 characters")
     private String name;
