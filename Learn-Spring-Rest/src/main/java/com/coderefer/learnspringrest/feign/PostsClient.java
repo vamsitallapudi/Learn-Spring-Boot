975fa64644d218f7d48e907d53fd63b4041812df
Vamsi Tallapudi <vamsi@coderefer.com>
2021-09-13 19:06:05 +0530

    spring cloud config server - now fetching data from git repo

--- Learn-Spring-Rest/src/main/java/com/coderefer/learnspringrest/feign/PostsClient.java
+++ Learn-Spring-Rest/src/main/java/com/coderefer/learnspringrest/feign/PostsClient.java
@@ -2,7 +2,6 @@ package com.coderefer.learnspringrest.feign;
 
 
 import com.coderefer.learnspringrest.entity.PostResponse;
-import com.coderefer.learnspringrest.entity.Student;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
