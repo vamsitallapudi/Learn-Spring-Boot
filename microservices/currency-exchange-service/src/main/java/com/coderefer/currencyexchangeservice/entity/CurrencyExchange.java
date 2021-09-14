6f4cd38d111f1eeee1c4846b11d1b9905d121290
Vamsi Tallapudi <vamsi@coderefer.com>
2021-09-14 22:15:56 +0530

    currency exchange service now working with h2 in memory database

--- microservices/currency-exchange-service/src/main/java/com/coderefer/currencyexchangeservice/entity/CurrencyExchange.java
+++ microservices/currency-exchange-service/src/main/java/com/coderefer/currencyexchangeservice/entity/CurrencyExchange.java
@@ -4,14 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class CurrencyExchange {
+    @Id
+    @GeneratedValue
     private Long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
---
+++ microservices/currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into CURRENCY_EXCHANGE (id, conversion_multiple, environment, currency_from, currency_to) values ( 1001, 71, '','USD','INR' );
+insert into CURRENCY_EXCHANGE (id, conversion_multiple, environment, currency_from, currency_to) values ( 1002, 65, '','USD','INR' );
