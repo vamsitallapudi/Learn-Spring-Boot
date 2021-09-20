921845b11b6ccf8ec2f38569c0cc039fb3e0c200
Vamsi Tallapudi <vamsi@coderefer.com>
2021-09-20 09:17:05 +0530

    adding proj related files

---
+++ LearnConcepts/src/test/java/com/coderefer/learnconcepts/LearnConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderefer.learnconcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnConceptsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
