11cc285d360ea8a27e4ae16241fcdea63f421ea8
Vamsi Tallapudi <mail4vamsikrishna@gmail.com>
2025-07-04 22:59:18 +0530

    updated the repo to use lombok

--- Learn-Spring-Rest/src/main/java/com/coderefer/learnspringrest/entity/Post.java
+++ Learn-Spring-Rest/src/main/java/com/coderefer/learnspringrest/entity/Post.java
@@ -1,80 +1,17 @@
 package com.coderefer.learnspringrest.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Post {
     private int id;
     private String title;
     private String body;
     private int userId;
-
-    public Post() {
-    }
-
-    public Post(int id, String title, String body, int userId) {
-        this.id = id;
-        this.title = title;
-        this.body = body;
-        this.userId = userId;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Post post = (Post) o;
-        return id == post.id &&
-                userId == post.userId &&
-                Objects.equals(title, post.title) &&
-                Objects.equals(body, post.body);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, title, body, userId);
-    }
-
-    @Override
-    public String toString() {
-        return "Post{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", body='" + body + '\'' +
-                ", userId=" + userId +
-                '}';
-    }
 }
--- Learn-Spring-Rest/src/main/java/com/coderefer/learnspringrest/entity/PostV2.java
+++ Learn-Spring-Rest/src/main/java/com/coderefer/learnspringrest/entity/PostV2.java
@@ -2,10 +2,17 @@ package com.coderefer.learnspringrest.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PostV2 extends Post {
     private String category;
@@ -23,143 +30,4 @@ public class PostV2 extends Post {
     private boolean isPublished;
     private String featuredImageUrl;
     private List<String> comments;
-
-    public PostV2() {
-        super();
-    }
-
-    public PostV2(int id, String title, String body, int userId, String category, String author,
-                  LocalDateTime createdAt, LocalDateTime updatedAt, List<String> tags, int likes, 
-                  int views, boolean isPublished, String featuredImageUrl, List<String> comments) {
-        super(id, title, body, userId);
-        this.category = category;
-        this.author = author;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.tags = tags;
-        this.likes = likes;
-        this.views = views;
-        this.isPublished = isPublished;
-        this.featuredImageUrl = featuredImageUrl;
-        this.comments = comments;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public List<String> getTags() {
-        return tags;
-    }
-
-    public void setTags(List<String> tags) {
-        this.tags = tags;
-    }
-
-    public int getLikes() {
-        return likes;
-    }
-
-    public void setLikes(int likes) {
-        this.likes = likes;
-    }
-
-    public int getViews() {
-        return views;
-    }
-
-    public void setViews(int views) {
-        this.views = views;
-    }
-
-    public boolean isPublished() {
-        return isPublished;
-    }
-
-    public void setPublished(boolean published) {
-        isPublished = published;
-    }
-
-    public String getFeaturedImageUrl() {
-        return featuredImageUrl;
-    }
-
-    public void setFeaturedImageUrl(String featuredImageUrl) {
-        this.featuredImageUrl = featuredImageUrl;
-    }
-
-    public List<String> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<String> comments) {
-        this.comments = comments;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        PostV2 postV2 = (PostV2) o;
-        return likes == postV2.likes &&
-                views == postV2.views &&
-                isPublished == postV2.isPublished &&
-                Objects.equals(category, postV2.category) &&
-                Objects.equals(author, postV2.author) &&
-                Objects.equals(createdAt, postV2.createdAt) &&
-                Objects.equals(updatedAt, postV2.updatedAt) &&
-                Objects.equals(tags, postV2.tags) &&
-                Objects.equals(featuredImageUrl, postV2.featuredImageUrl) &&
-                Objects.equals(comments, postV2.comments);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), category, author, createdAt, updatedAt, tags, likes, views, isPublished, featuredImageUrl, comments);
-    }
-
-    @Override
-    public String toString() {
-        return "PostV2{" +
-                "category='" + category + '\'' +
-                ", author='" + author + '\'' +
-                ", createdAt=" + createdAt +
-                ", updatedAt=" + updatedAt +
-                ", tags=" + tags +
-                ", likes=" + likes +
-                ", views=" + views +
-                ", isPublished=" + isPublished +
-                ", featuredImageUrl='" + featuredImageUrl + '\'' +
-                ", comments=" + comments +
-                "} " + super.toString();
-    }
 } 
